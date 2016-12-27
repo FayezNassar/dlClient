ea159f770c9aca08058677b417ab10291498b32a
Fayez Nassar <fayeznas@gmail.com>
2016-12-27 22:04:27 +0200

    1.add get_start(): the client send to the server a request for join the system, and he expect to receive back from the server a id. 2.the client send request to the server to send him work to do, and he expect to receive back what to do in the mode field: train, validate or done. if a. done, he will exist the system b.if train,he will train with the received network on the image file he received c. if validate, he will test the network he received with some images files.

--- Client.py
+++ Client.py
@@ -12,42 +12,48 @@ import json
 import time
 
 
-lin_neural_network_l1 = L.Linear(784, 300)
-lin_neural_network_l2 = L.Linear(300, 10)
-mlp = None
-
-
-def main():
+def get_start():
     conn = http.client.HTTPConnection("localhost", 8000)
-    conn.request("GET", "/hello/requestWork")
+    conn.request("POST", "/hello/joinSystem", '')
     response = conn.getresponse()
-    response_message = response.read().decode('utf-8')
-    conn.close()
-    image_file_index = json.loads(response_message)['image_file_index']
-    mode = str(json.loads(response_message)['mode'])
-    l1_w_list = json.loads(response_message)['l1_w']
-    l2_w_list = json.loads(response_message)['l2_w']
-
-    for i in range(300):
-        for j in range(784):
-            lin_neural_network_l1.W.data[i][j] = l1_w_list[i][j]
-    for i in range(10):
-        for j in range(300):
-            lin_neural_network_l2.W.data[i][j] = l2_w_list[i][j]
-
-    global mlp
-    mlp = MLP(lin_neural_network_l1, lin_neural_network_l2)
-    if (mode == "train"):
-        train(image_file_index, l1_w_list, l2_w_list)
-    if (mode == "validation"):
-        validate()
+    _client_id = str(response.read().decode('utf-8'))
+    return int(_client_id)
 
 
-def train(image_file_index, l1_w_list, l2_w_list):
+def main(client_id):
+    while True:
+        conn = http.client.HTTPConnection("localhost", 8000)
+        conn.request("GET", "/hello/deepLearning")
+        response = conn.getresponse()
+        response_message = response.read().decode('utf-8')
+        conn.close()
+        image_file_index = json.loads(response_message)['image_file_index']
+        mode = str(json.loads(response_message)['mode'])
+        if mode == 'done':
+            return
+
+        l1_w_list = json.loads(response_message)['l1_w']
+        l2_w_list = json.loads(response_message)['l2_w']
+        lin_neural_network_l1 = L.Linear(784, 300)
+        lin_neural_network_l2 = L.Linear(300, 10)
+        for i in range(300):
+            for j in range(784):
+                lin_neural_network_l1.W.data[i][j] = l1_w_list[i][j]
+        for i in range(10):
+            for j in range(300):
+                lin_neural_network_l2.W.data[i][j] = l2_w_list[i][j]
+        mlp = MLP(lin_neural_network_l1, lin_neural_network_l2)
+
+        if mode == "train":
+            train(client_id, mlp, image_file_index, l1_w_list, l2_w_list)
+        if mode == "validation":
+            validate(client_id, mlp)
+
+
+def train(client_id, mlp, image_file_index, l1_w_list, l2_w_list):
     print("working in file: " + str(image_file_index))
     file_images_name = '~/images_train/image_' + str(image_file_index)
     file_labels_name = '~/labels_train/label_' + str(image_file_index)
-    global mlp
     # Create a model
     model = L.Classifier(mlp)
     # Setup an optimizer
@@ -99,22 +105,25 @@ def train(image_file_index, l1_w_list, l2_w_list):
         for j in range(784):
             delta_layer1[i][j] = mlp.l1.W.data[i][j] - l1_w_list[i][j];
 
-    print(type(delta_layer1))
-    print(type(delta_layer1.tolist()))
     data = {
+        'id': client_id,
+        'mode': 'train',
         'work_time': total_time,
-        # 'l1_delta': delta_layer1.tolist(),
-        # 'l2_delta': delta_layer2.tolist()
+        'l1_delta': delta_layer1.tolist(),
+        'l2_delta': delta_layer2.tolist()
     }
     print("total_time: " + str(total_time))
     conn = http.client.HTTPConnection("localhost", 8000)
-    conn.request("GET", "/hello/responseResult", json.dumps(data))
+    conn.request("POST", "/hello/deepLearning", json.dumps(data))
+    conn.getresponse()
     conn.close()
 
 
-def validate():
+def validate(client_id, mlp):
+
     return 0
 
 
 if __name__ == '__main__':
-    main()
+    client_id = get_start()
+    main(client_id)
